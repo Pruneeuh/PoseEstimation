222298185ad389074c5988dc7e9910dcdfdde58d
Prune <prune.mamalet@etu.inp-n7.fr>
2025-06-24 14:38:59 +0200

    commentaires test

--- P3P_openCV_numpy/test_numpy.py
+++ P3P_openCV_numpy/test_numpy.py
@@ -2,19 +2,23 @@ from initialisation_parametres import *
 from P3P_numpy import P3P
 from test import *
 
-
-
-
-A = camera()
+# Generate the camera matrixs
+A = camera()  # intraseca
 R = rotation_matrix()
 T = camera_translation()
 
+# Generate the 3D points 
 points3D_4 = pts_3D_4pts()
 points3D = pts_3D_3pts(points3D_4)
+
+# Generate the 2D points
 points2D_4 = pts_2D_4pts(points3D_4,T,R,A)
 
+# Generate the features vectors : fi = TPi / ||TPi||
 features_vectors = features_vectors(points3D,T)
 
+# Recover the P3P solutions 
 solution_numpy = P3P(points3D,features_vectors)
 
+# Computation of the errors 
 affichage_erreur(solution_numpy,points2D_4,points3D_4,A)
--- P3P_openCV_numpy/test_openCV.py
+++ P3P_openCV_numpy/test_openCV.py
@@ -2,16 +2,22 @@ from initialisation_parametres import *
 from P3P_openCV import *
 from test import *
 
-
-A = camera()
-R = rotation_matrix()
+# Generate the camera matrixs
+A = camera()        # intraseca
+R = rotation_matrix()    
 T = camera_translation()
 
+
+# Generate the 3D points 
 points3D_4 = pts_3D_4pts()
 points3D = pts_3D_3pts(points3D_4)
+
+# Generate the 2D points
 points2D_4 = pts_2D_4pts(points3D_4,T,R,A)
 points2D = pts_2D_3pts(points2D_4)
 
+# Recover the P3P solutions 
 solution_openCV = recup_solutions_openCV(points2D_4,points3D_4,A)
 
+# Computation of the errors 
 affichage_erreur(solution_openCV,points2D_4,points3D_4,A)
